96251169069f843bc84f120abcf02c2dc4441f90
meinou <mskozlovskaya@gmail.com>
2018-06-11 12:50:03 -0400

    twitter routing

--- boot-social-api-master/src/main/java/social/social/CustomWebMvcConfigurerAdapter.java
+++ boot-social-api-master/src/main/java/social/social/CustomWebMvcConfigurerAdapter.java
@@ -6,14 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class CustomWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("").setViewName("forward:/app/index.html");
-        registry.addViewController("/").setViewName("forward:/app/index.html");
-        registry.addViewController("/app").setViewName("redirect:/app/");
-        registry.addViewController("/app/").setViewName("forward:/app/index.html");
-      //  registry.addViewController("*").setViewName("forward:/app/index.html");
-        super.addViewControllers(registry);
-    }
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("").setViewName("forward:/app/index.html");
+//        registry.addViewController("/").setViewName("forward:/app/index.html");
+//        registry.addViewController("/app").setViewName("redirect:/app/");
+//        registry.addViewController("/app/").setViewName("forward:/app/index.html");
+//      //  registry.addViewController("*").setViewName("forward:/app/index.html");
+//        super.addViewControllers(registry);
+//    }
 }
